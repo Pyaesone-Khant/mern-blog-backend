cd192341216e7bf0159164fa8b6797ee2ff20ded
Pyaesone Khant <pyaepsk@gmail.com>
2025-04-22 11:46:09 +0630

    feat: transform created conversation object

--- controllers/ConversationController.js
+++ controllers/ConversationController.js
@@ -4,20 +4,34 @@ const { ConversationServices, UserServices } = require("../services");
 const createNewConversation = async (req, res) => {
     try {
         const userId = req.userId;
-        const { receiver } = req.body;
+        const { receiver: receiverId } = req.body;
 
-        if (!receiver) return ResponseObj(res, 400, { message: "Receiver ID is required!" });
+        if (!receiverId) return ResponseObj(res, 400, { message: "Receiver ID is required!" });
 
         const payload = [
             userId,
-            receiver,
-        ]
+            receiverId,
+        ];
 
         const newConversation = await ConversationServices.createConversation(payload);
-        return ResponseObj(res, 200, {
-            message: "New conversation has been created.",
-            data: newConversation,
-        });
+        const sender = await UserServices.findUserByColumn({ _id: userId });
+        const receiver = await UserServices.findUserByColumn({ _id: receiverId });
+
+        const conversation = {
+            ...newConversation?.toObject(),
+            sender: {
+                _id: sender?._id,
+                name: sender?.name,
+                profileImage: transformImageUrl(sender?.profileImage),
+            },
+            receiver: {
+                _id: receiver?._id,
+                name: receiver?.name,
+                profileImage: transformImageUrl(receiver?.profileImage),
+            },
+        };
+
+        return ResponseObj(res, 200, conversation);
     } catch (error) {
         return ResponseObj(res, 500, { message: error?.message || "Internal Server Error!" });
     }
